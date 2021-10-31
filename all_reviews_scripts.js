9c83d95313d50ceb23efae84fa8383244d420703
Greg <Greggomez11235@gmail.com>
2021-10-31 17:23:03 -0500

    Changed field names of all_reviews_scripts.js from Camel Casing to Underscore Casing

--- all_reviews_scripts.js
+++ all_reviews_scripts.js
@@ -26,73 +26,73 @@ consumerCursor.forEach(function(myDoc){
 
 db.review_for_producer.insertMany([
     {
-        "consumerId": consumerMap.get('Octovio'),
-        "producerId": producerMap.get('Bob'),
+        "consumer_id": consumerMap.get('Octovio'),
+        "producer_id": producerMap.get('Bob'),
         "rating": 5,
         "title": 'Excellent Service',
         "description": 'Morbi rutrum metus in risus luctus, id porttitor ex euismod. Phasellus augue erat, tempus ut lorem vel, tristique cursus odio. Integer convallis orci in mauris consectetur',
-        "dateCreated": new Date()
+        "date_created": new Date()
     },
     {
-        "consumerId": consumerMap.get('Octovio'),
-        "producerId": producerMap.get('Jenna'),
+        "consumer_id": consumerMap.get('Octovio'),
+        "producer_id": producerMap.get('Jenna'),
         "rating": 5,
         "title": 'Wow, Jenna\'s service was impeccable',
         "description": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies gravida arcu id fermentum. Pellentesque ac luctus est, malesuada sodales purus. Etiam lorem quam, blandit in auctor dapibus, fermentum eget velit. ',
-        "dateCreated": new Date()
+        "date_created": new Date()
     },
     {
-        "consumerId": consumerMap.get('Jose'),
-        "producerId": producerMap.get('Bob'),
+        "consumer_id": consumerMap.get('Jose'),
+        "producer_id": producerMap.get('Bob'),
         "rating": 1,
         "title": 'WTF?????',
         "description": 'Trash',
-        "dateCreated": new Date()
+        "date_created": new Date()
     },
     {
-        "consumerId": consumerMap.get('Jose'),
-        "producerId": producerMap.get('Marietta'),
+        "consumer_id": consumerMap.get('Jose'),
+        "producer_id": producerMap.get('Marietta'),
         "rating": 3,
         "title": 'Decent',
         "description": 'It was okay, my dog seemed to enjoy it.',
-        "dateCreated": new Date()
+        "date_created": new Date()
     }
 ]);
 
 db.review_for_consumer.insertMany([
     {
-        "consumerId": consumerMap.get('Octovio'),
-        "producerId": producerMap.get('Bob'),
+        "consumer_id": consumerMap.get('Octovio'),
+        "producer_id": producerMap.get('Bob'),
         "rating": 5,
         "title": 'Pleasure doing business with',
         "description": 'Was on time',
-        "dateCreated": new Date()
+        "date_created": new Date()
     },
     {
-        "consumerId": consumerMap.get('Jose'),
-        "producerId": producerMap.get('Bob'),
+        "consumer_id": consumerMap.get('Jose'),
+        "producer_id": producerMap.get('Bob'),
         "rating": 1,
         "title": 'Horrible experience',
         "description": 'Smelled',
-        "dateCreated": new Date()
+        "date_created": new Date()
     },
 ]);
 
 db.review_for_food.insertMany([
     {
-        "foodId": foodMap.get('Jenna\'s Vegan Blueberry Cookies')._id,
-        "consumerId": consumerMap.get('Octovio'),
+        "food_id": foodMap.get('Jenna\'s Vegan Blueberry Cookies')._id,
+        "consumer_id": consumerMap.get('Octovio'),
         "rating": 5,
         "title": "Best cookies ever",
         "description": 'The texture. The taste. My satisfactory face, 10/10',
-        "dateCreated": new Date()
+        "date_created": new Date()
     },
     {
-        "foodId": foodMap.get('Jenna\'s Vegan Blueberry Cookies')._id,
-        "consumerId": consumerMap.get('Jose'),
+        "food_id": foodMap.get('Jenna\'s Vegan Blueberry Cookies')._id,
+        "consumer_id": consumerMap.get('Jose'),
         "rating": 5,
         "title": "Vegan????",
         "description": 'I\'m amazed that a vegan cookie can taste so good. Gotta try it to belive it! ',
-        "dateCreated": new Date()
+        "date_created": new Date()
     },
 ])
