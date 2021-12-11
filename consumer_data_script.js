8d03ac0293aa8ebaea50d3a254dced4460e829e1
Rutvik Patel <rutvik503@gmail.com>
2021-12-11 02:50:13 -0600

    added photo, bio and email_address fields to consumer and producer collections

--- consumer_data_script.js
+++ consumer_data_script.js
@@ -8,6 +8,9 @@ db.consumer.insertOne(
         first_name: "Juan",
         last_name: "Lopez",    
         phone_number: "8157890123",
+        email_address: "jlopez@gmail.com",
+        photo: "http://www.juan.com",
+        bio: "Hello, this is bm bio. In here I say stuff that I want others to know.",
         address: {
             street: "656 Glidden Ave",
             city: "DeKalb",
@@ -30,6 +33,9 @@ db.consumer.insertOne(
         first_name: "Gio",
         last_name: "Gonzalez",    
         phone_number: "8157090123",
+        email_address: "ggonzalez@gmail.com",
+        photo: "http://www.gio.com",
+        bio: "Hi, I am a student at NIU and I am not sure why I am saying this here but whatever.",
         address: {
             street: "60 N Nina St",
             city: "Cortland",
@@ -51,6 +57,9 @@ db.consumer.insertOne(
         first_name: "Jose",
         last_name: "Hernandez",    
         phone_number: "8157096489",
+        email_address: "jhernandez@gmail.com",
+        photo: "http://www.jose.com",
+        bio: "Good day, I was created by greg and he gave me the name. Now sure what my purpose is here!",
         address: {
             street: "26 N Nina St",
             city: "Cortland",
@@ -72,6 +81,9 @@ db.consumer.insertOne(
         first_name: "Octovio",
         last_name: "Ramirez",    
         phone_number: "8158196927",
+        email_address: "oramirez@gmail.com",
+        photo: "http://www.octovio.com",
+        bio: "Bad day, I was created by rut and he didn't gave me this name. I wonder who did tho!",
         address: {
             street: "306 W Hillcrest Dr",
             city: "DeKalb",
@@ -93,6 +105,9 @@ db.consumer.insertOne(
         first_name: "Antony",
         last_name: "Marc",    
         phone_number: "8159004797",
+        email_address: "amarc@gmail.com",
+        photo: "http://www.antony.com",
+        bio: "Listen, feel like whoever names me spelled my name wrong, I feel like it should be Anthony.",
         address: {
             street: "1490 Farmstead Ln",
             city: "Malta",
